5a5acb8bf36003e9f5d612f83f605c93f2da57ca
Megan McCullough <mmccullough206@gmail.com>
2019-04-06 21:59:43 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -15,15 +15,16 @@ def display_board(board)
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
-  
+
 end
 
 def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def move(board, index, current_player)
-  board[index] = current_player
+def move(current_player)
+  
+  board[index] = character
 end
 
 def position_taken?(board, location)
@@ -38,11 +39,10 @@ def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
-  if valid_move?(board, index) == true
+  if valid_move?(board, index)
     move(board, index, current_player(board))
     display_board(board)
-  else valid_move?(board, index) == false
-    until valid_move?(board, index) == true
-      input = gets.strip
+  else
+    turn(board)
   end
 end
